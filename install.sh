9b6869f4175db8b607049d5e30ddd6de0daa999a
SimoneFassio <fassio.simone@gmail.com>
2025-03-26 12:31:10 +0100

    fix container installation

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 python3 -m venv venv
 source ./venv/bin/activate
-venv/bin/pip install -r 'requirements.txt'
+venv/bin/pip install --no-warn-script-location -r 'requirements.txt'
 cd ./static
 npm install
 cd ..
