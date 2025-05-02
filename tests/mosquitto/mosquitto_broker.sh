0ff737b0cce469d58663b6eb3ab2ab3bf35d866c
Sampli1 <sandro.marghella@gmail.com>
2025-05-02 14:13:29 +0200

    fixed tests environment

--- tests/mosquitto/mosquitto_broker.sh
+++ tests/mosquitto/mosquitto_broker.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR=$(dirname "$0")
 sudo mkdir -p /var/run/mosquitto
 sudo chown mosquitto:mosquitto /var/run/mosquitto
 
+sudo touch /var/log/mosquitto/mosquitto.log
+
 # Start mosquitto with the local configuration file
 echo "Starting mosquitto using ./mosquitto.conf..."
 sudo mosquitto -v -c "$SCRIPT_DIR/mosquitto.conf"
